1afef9263afade026c198505723ed45be44003c2
Zrinszki Ármin <zrinszki.armin@gmail.com>
2024-10-19 11:09:20 +0200

    Creadet scraping command

--- factChecker/models/article.py
+++ factChecker/models/article.py
@@ -1,9 +1,5 @@
 from django.db import models
-from .iptc_code import IPTCCode
-from .geographic_code import GeographicCode
-from .sab_code import SABCode
-from .person import Person
-from .institution import Institution
+from .keyword import Keyword
 
 class Article(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,52 +8,17 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     lead = models.TextField()
     text = models.TextField()
-    iptc_codes = models.ManyToManyField(IPTCCode, through='ArticleIPTCCode', related_name='articles')
-    geographic_codes = models.ManyToManyField(GeographicCode, through="ArticleGeographicCode", related_name='articles')
-    sab_codes = models.ManyToManyField(SABCode, through="ArticleSABCode", related_name='articles')
-    persons = models.ManyToManyField(Person, through='ArticlePerson', related_name='articles')
-    institutions = models.ManyToManyField(Institution, through='ArticleInstitution', related_name='articles')
+    keywords = models.ManyToManyField(Keyword, through='ArticleKeyword', related_name='articles')
     link = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-class ArticleIPTCCode(models.Model):
+class ArticleKeyword(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    iptc_code = models.ForeignKey(IPTCCode, on_delete=models.CASCADE)
-    weight = models.IntegerField()
+    keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE)
+    weight = models.IntegerField(default=0)
 
     class Meta:
-        unique_together = ('article', 'iptc_code')
-
-class ArticlePerson(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    weight = models.IntegerField()
-
-    class Meta:
-        unique_together = ('article', 'person')
-
-class ArticleInstitution(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
-    weight = models.IntegerField()
-
-    class Meta:
-        unique_together = ('article', 'institution')
-
-class ArticleGeographicCode(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    geographic_code = models.ForeignKey(GeographicCode, on_delete=models.CASCADE)
-    weight = models.IntegerField()
-
-    class Meta:
-        unique_together = ('article', 'geographic_code')
-
-class ArticleSABCode(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    sab_code = models.ForeignKey(SABCode, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('article', 'sab_code')
+        unique_together = ('article', 'keyword')
--- factChecker/models/keyword.py
+++ factChecker/models/keyword.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class IPTCCode(models.Model):
+class Keyword(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    weight = models.IntegerField(help_text="Percentage value associated with this IPTC code")
+    weight = models.IntegerField(help_text="Percentage value associated with this keyword")
 
     def __str__(self):
         return f"{self.name} ({self.weight}%)"
