72625dcbc0099d8d7804d57b41156bc9cd493fe9
smokk <pdc.levike@gmail.com>
2024-10-21 22:36:49 +0200

    fixed links varchar length

--- factChecker/models/link.py
+++ factChecker/models/link.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Link(models.Model):
-    url = models.URLField(unique=True)
+    url = models.URLField(max_length=400, unique=True)
     scraped = models.BooleanField(default=False)
 
     def __str__(self):
