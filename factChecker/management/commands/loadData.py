9fef1825edc40f88b38edeb8200968a9c2403018
Zrinszki Ármin <zrinszki.armin@gmail.com>
2024-10-28 22:20:27 +0100

    embed articles

--- factChecker/management/commands/loadData.py
+++ factChecker/management/commands/loadData.py
@@ -3,7 +3,13 @@ from llama_index import (
     download_loader,
     Document,
 )
+from llama_index.embeddings import HuggingFaceEmbeddings
 from django.core.management.base import BaseCommand
+from factChecker.models import Article
+
+EMBED_MODEL = HuggingFaceEmbeddings(
+    model_name="WhereIsAI/UAE-Large-V1", embed_batch_size=10  # open-source embedding model
+)
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
@@ -19,6 +25,7 @@ class Command(BaseCommand):
         parsed_url = urlparse("postgresql://szakdoga:Kutyakutya1@localhost:5432/archivum")
         documents = self.fetch_documents_from_storage(query=query, parsed_url=parsed_url)
         print(f"Found {len(documents)} documents")
+        self.save_embeddings(documents)
 
     def fetch_documents_from_storage(self, query: str, parsed_url) -> list[Document]:
         # Prep documents - fetch from DB
@@ -32,3 +39,16 @@ class Command(BaseCommand):
             dbname=parsed_url.path[1:],  # Database Name
         )
         return reader.load_data(query=query)
+    
+    def save_embeddings(documents: list[Document]) -> None:
+        for document in documents:
+
+            # Get the article
+            article = Article.objects.get(id=document.id)
+
+            # Get the embedding
+            embedding = EMBED_MODEL.embed(document.text)
+
+            # Save the embedding
+            article.embedding = embedding
+            article.save()
